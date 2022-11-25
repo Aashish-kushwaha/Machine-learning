759468ac05bf72ed4c62eaca03ec5b2528dc92b1
Aashish-kushwaha <kushwahaashu444@gmail.com>
2022-11-26 00:11:58 +0530

    pandas starts

---
+++ pandassss.py
@@ -0,0 +1,46 @@
+import pandas as pd
+import numpy as np
+
+data={
+    'roll_no':[1,2,3,4],
+    'prr_id':[34,56,67,89],
+    'marks':[12,34,556,78]
+}
+print(data)
+df1=pd.DataFrame(data)
+print(df1)
+
+# setting index
+df2=pd.DataFrame(data,index=['a','b','c','d'])
+print(df2)
+
+print(df2.loc['a'])
+print(df2.iloc[0:2,2:3])
+
+# loading csv file
+df=pd.read_csv("iris.csv")
+print(df.head())
+
+print(df.info())
+print(df.describe())
+
+# missingvalues
+data={
+    'roll_no':[1,2,3,4],
+    'prr_id':[34,56,67,89],
+    'marks':[np.nan,34,556,78]
+}
+df1=pd.DataFrame(data)
+print(df1)
+
+print(df1.isnull())
+print(df1.isnull().sum())
+
+df2=df1.fillna(1)
+print(df2)
+
+# drop null values
+a=df1.dropna()  #removes the entire row
+print(a)
+
+
