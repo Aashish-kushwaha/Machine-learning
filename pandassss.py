f14a7150278849575bf924fc174ba907550c967c
Aashish-kushwaha <kushwahaashu444@gmail.com>
2023-04-16 19:24:57 +0530

    pandas continue

--- pandassss.py
+++ pandassss.py
@@ -19,28 +19,55 @@ print(df2.iloc[0:2,2:3])
 
 # loading csv file
 df=pd.read_csv("iris.csv")
+print(df)
+
+#printing starting value of file
 print(df.head())
 
+#top 3 rows
+print(df.head(3))
+
+#last value of data
+print(df.tail())
+
+#size of data
+print(df.shape)
+
+#information of data
 print(df.info())
+
+#used to give mathematical summary work only on numerical columns
 print(df.describe())
 
-# missingvalues
-data={
-    'roll_no':[1,2,3,4],
-    'prr_id':[34,56,67,89],
-    'marks':[np.nan,34,556,78]
-}
-df1=pd.DataFrame(data)
-print(df1)
 
-print(df1.isnull())
-print(df1.isnull().sum())
+#fetch column
+sr=df['sepal.length']
+print(sr)
+print(type(sr))
 
-df2=df1.fillna(1)
-print(df2)
+#fetch multiple column:we pass the  list 
+li=df[['sepal.length','sepal.width','petal.length']]
+print(li)
+print(type(li))
 
-# drop null values
-a=df1.dropna()  #removes the entire row
-print(a)
+
+# # missingvalues
+# data={
+#     'roll_no':[1,2,3,4],
+#     'prr_id':[34,56,67,89],
+#     'marks':[np.nan,34,556,78]
+# }
+# df1=pd.DataFrame(data)
+# print(df1)
+
+# print(df1.isnull())
+# print(df1.isnull().sum())
+
+# df2=df1.fillna(1)
+# print(df2)
+
+# # drop null values
+# a=df1.dropna()  #removes the entire row
+# print(a)
 
 
